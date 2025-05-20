051c8f0f9241d08a9c29439717d27a590a76c8be
Robinhokcond1 <robinhokcond1@hotmail.com>
2025-05-20 11:35:44 -0300

    feat: adiciona repositórios JPA para as entidades do domínio

--- src/main/java/com/estapar/estacionamento/repository/ParkingSessionRepository.java
+++ src/main/java/com/estapar/estacionamento/repository/ParkingSessionRepository.java
@@ -4,7 +4,6 @@ import com.estapar.estacionamento.model.ParkingSession;
 import com.estapar.estacionamento.model.Sector;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
